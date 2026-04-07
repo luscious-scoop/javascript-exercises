defcfc3b66b7683a3e0742f73b8569c4f23924bc
Haseeb <ah085904@gmail.com>
2026-04-07 20:27:23 +0500

    the correct solution

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,15 +1,8 @@
 const removeFromArray = function (array, ...items) {
   let unique = [];
   for (let i = 0; i < array.length; i++) {
-    if (!unique.includes(array[i])) {
+    if (!items.includes(array[i])) {
       unique.push(array[i]);
-    }
-  }
-
-  for (let i = 0; i < items.length; i++) {
-    let index = unique.indexOf(items[i]);
-    if (index !== -1) {
-      unique.splice(index, 1);
     }
   }
   return unique;
