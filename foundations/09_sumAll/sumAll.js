0dc9081f23ff09cd801ac920285a9332470d621f
Haseeb <ah085904@gmail.com>
2026-04-07 23:22:59 +0500

    no commit message

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (...args) {
+  let min = Math.min(...args);
+  let max = Math.max(...args);
+  let sum = 0;
+  if (
+    min < 0 ||
+    !Number.isInteger(min) ||
+    !Number.isInteger(max) ||
+    !typeof min === "number" ||
+    !typeof max === "number"
+  ) {
+    return "ERROR";
+  }
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
