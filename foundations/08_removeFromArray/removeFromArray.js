24adcc20e67b866e58b85eddc94b41132bb9b45b
Haseeb <ah085904@gmail.com>
2026-04-07 16:33:12 +0500

    wrote from from array function uing for loops and splice

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...items) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < items.length; j++) {
+      if (items[j] === array[i]) {
+        array.splice(i, 1);
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
