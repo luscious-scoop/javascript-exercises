35001f097b6972c7196e542174baa47346632236
Haseeb <ah085904@gmail.com>
2026-04-08 15:20:59 +0500

    wrote function for conversion for cesius and fahrenheit

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  let celsius = (temperature - 32) * (5 / 9);
+  if (celsius === 0) {
+    return celsius;
+  } else {
+    return +celsius.toFixed(1);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  let fahrenheit = temperature * (9 / 5) + 32;
+  return +fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
