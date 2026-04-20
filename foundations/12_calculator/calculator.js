ee0e368f76a919e7f6605f89429ff3a902bd4a62
Haseeb <ah085904@gmail.com>
2026-04-20 18:13:33 +0500

    sum function completed

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -2,9 +2,16 @@ const add = function (a, b) {
   return a + b;
 };
 
-const subtract = function () {};
+const subtract = function (a, b) {
+  return a - b;
+};
 
-const sum = function () {};
+const sum = function (array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return array.reduce((total, current) => total + current, 0);
+};
 
 const multiply = function () {};
 
