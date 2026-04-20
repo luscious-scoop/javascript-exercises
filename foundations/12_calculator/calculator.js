41e81a6cf62cbee8420bc0ace69a451828616a78
Haseeb <ah085904@gmail.com>
2026-04-20 18:19:45 +0500

    power function done

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -13,9 +13,13 @@ const sum = function (array) {
   return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function () {};
+const multiply = function (array) {
+  return array.reduce((total, current) => total * current, 1);
+};
 
-const power = function () {};
+const power = function (base, power) {
+  return Math.pow(base, power);
+};
 
 const factorial = function () {};
 
