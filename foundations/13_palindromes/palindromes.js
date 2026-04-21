e3e6054a7735f47a15c69f9f672bdefa2cdfce66
Haseeb <ah085904@gmail.com>
2026-04-21 21:24:21 +0500

    exercise done

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,36 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let array = string;
+  let index;
+  array = array.toLowerCase();
+  array = array.trim();
+  if (array.includes(",") || array.includes(" ")) {
+    array = array.split("");
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === ",") {
+        array.splice(i, 1);
+      }
+      if (array[i] === " ") {
+        array.splice(i, 1);
+      }
+    }
+  } else {
+    array = array.split("");
+  }
+  if (array.includes(".")) {
+    index = array.indexOf(".");
+    array.splice(index, 1);
+  } else if (array.includes("!")) {
+    index = array.indexOf("!");
+    array.splice(index, 1);
+  }
+  let reversed = [...array].reverse();
+  array = array.join("");
+  reversed = reversed.join("");
 
+  if (array === reversed) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
