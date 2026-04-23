cbd83276e3ba9a1a8dd2142faa79d6d5de210182
Haseeb <ah085904@gmail.com>
2026-04-23 15:53:18 +0500

    15th exercise done

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function (array) {
+  let arr = [];
 
+  for (let i = 0; i < array.length; i++) {
+    arr.push(array[i].title);
+  }
+  return arr;
 };
 
 // Do not edit below this line
