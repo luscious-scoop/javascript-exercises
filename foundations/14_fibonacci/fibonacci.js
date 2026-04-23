f5d2489a9ea0ba5f2ef3ea96d87c34f03055d337
Haseeb <ah085904@gmail.com>
2026-04-23 15:25:49 +0500

    finally 14th exercise done

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let array = [0, 1, 1, 2, 3, 5, 8, 13]; //[0,1,1,2,3,5,8,13]
 
+  if (num < 0) {
+    return "OOPS";
+  }
+
+  if (num < array.length) {
+    return array[num];
+  } else {
+    for (let i = array.length - 1; i < num; i++) {
+      //7
+      result = array[i] + array[i - 1];
+      array.push(result);
+    }
+    return array[array.length - 1];
+  }
 };
 
 // Do not edit below this line
