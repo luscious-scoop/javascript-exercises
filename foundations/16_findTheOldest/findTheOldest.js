031401f4aa6616a69137cbb8ecfcadaeb18f407a
Haseeb <ah085904@gmail.com>
2026-04-23 16:22:02 +0500

    last exercise done

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (arrayOfPeople) {
+  let age = [];
 
+  for (let i = 0; i < arrayOfPeople.length; i++) {
+    if (arrayOfPeople[i].yearOfDeath === undefined) {
+      arrayOfPeople[i].yearOfDeath = new Date().getFullYear();
+    }
+    age.push(arrayOfPeople[i].yearOfDeath - arrayOfPeople[i].yearOfBirth);
+  }
+  let max = Math.max(...age);
+  let index = age.indexOf(max);
+  return arrayOfPeople[index];
 };
 
 // Do not edit below this line
