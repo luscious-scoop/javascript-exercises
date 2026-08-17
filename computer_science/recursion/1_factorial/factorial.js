0a9a2a1abb1346fd7f9ee43c53603d727bd86ee6
Haseeb <ah085904@gmail.com>
2026-08-17 17:22:05 +0500

    recursion exercise done

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,17 @@
-const factorial = function() {
+const factorial = function (n) {
+	if (
+		typeof n === 'string' ||
+		Number.isInteger(n) === false ||
+		Array.isArray(n) ||
+		n < 0
+	) {
+		return;
+	}
+	if (n === 0) {
+		return 1;
+	}
 
+	return n * factorial(n - 1);
 };
 
 // Do not edit below this line
