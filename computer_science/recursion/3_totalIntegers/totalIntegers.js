80f2e162a9e5f83e0a6bf01678a7ddb7c9c14f25
Haseeb <ah085904@gmail.com>
2026-08-20 18:29:24 +0500

    try this method works for now

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,17 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (object, count = 0, n = 1) {
+	const values = Object.values(object);
+
+	values.forEach((item) => {
+		if (Number.isInteger(item)) {
+			count++;
+		} else if (typeof item === 'object' && item !== null) {
+			count = totalIntegers(item, count, n + 1);
+		}
+	});
+	return count;
 };
-  
+
+console.log(totalIntegers([4, 6, { a: 1, b: { a: [5, 10], b: 11 } }, 9]));
+
 // Do not edit below this line
 module.exports = totalIntegers;
