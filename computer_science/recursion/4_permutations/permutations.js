b5a3e776a933d3ae6703665a8bf54ccdbf43708d
Haseeb <ah085904@gmail.com>
2026-08-23 15:01:14 +0500

    write a function to check the permuted array already contains the array

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -25,13 +25,13 @@ const permutations = function (
 
 		if (secondValue >= array.length) {
 			secondValue = secondValue % array.length;
+		} else {
+			[array[firstValue], array[secondValue]] = [
+				array[secondValue],
+				array[firstValue],
+			];
+			firstValue = secondValue;
 		}
-
-		[array[firstValue], array[secondValue]] = [
-			array[secondValue],
-			array[firstValue],
-		];
-		firstValue = secondValue;
 	}
 
 	return permutations(
@@ -47,7 +47,35 @@ const permutations = function (
 
 // [1,2,3]= [[1,2,3] , [2,1,3], [3,1,2]. [1,3,2], [2,3,1], [3,2,1]]
 
-console.log(permutations([1, 2, 3]));
+// console.log(permutations([1, 2, 3]));
+
+function doesContainTheArray(array, array2) {
+	let doesContain = false;
+	for (let i = 0; i < array2.length; i++) {
+		for (let j = 0; j < array2[i].length; j++) {
+			if (array[j] !== array2[i][j]) {
+				doesContain = false;
+			} else {
+				doesContain = true;
+			}
+		}
+		if (doesContain) {
+			return doesContain;
+		}
+	}
+
+	return true;
+}
+
+let array = [1, 2, 3];
+
+let array2 = [
+	[1, 4, 3],
+	[9, 5, 6],
+	[1, 2, 3],
+];
+
+console.log(doesContainTheArray(array, array2));
 
 // Do not edit below this line
-module.exports = permutations;
+// module.exports = permutations;
