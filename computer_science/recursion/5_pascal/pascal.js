f716d637032133151826a877983a80168d817fc6
Haseeb <ah085904@gmail.com>
2026-08-25 14:03:09 +0500

    try this code

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,15 @@
-const pascal = function() {
-  
+const pascal = function (n, results = [1], index = 0, valueToAdd = 0) {
+	if (n === 1) {
+		return results;
+	}
+	valueToAdd = results[index] + valueToAdd;
+
+	results.splice(index, 0, valueToAdd);
+
+	return pascal(n - 1, results, index + 1, valueToAdd);
 };
-  
+
+console.log(pascal(4));
+
 // Do not edit below this line
 module.exports = pascal;
