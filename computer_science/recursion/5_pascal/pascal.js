ac42a3272929062d0a0a572d22f81e8a96440336
Haseeb <ah085904@gmail.com>
2026-08-25 17:49:51 +0500

    trying this code

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,15 +1,29 @@
-const pascal = function (n, results = [1], index = 0, valueToAdd = 0) {
+const pascal = function (
+	n,
+	results = [1],
+	sampleArray = [0, 1, 0],
+	index = 0,
+	secondIndex = 1,
+) {
 	if (n === 1) {
 		return results;
 	}
-	valueToAdd = results[index] + valueToAdd;
 
-	results.splice(index, 0, valueToAdd);
+	let valueToAdd = sampleArray[index] + sampleArray[secondIndex];
+	results.splice(secondIndex, 0, valueToAdd);
 
-	return pascal(n - 1, results, index + 1, valueToAdd);
+	sampleArray = [...results];
+	let temp = index;
+	index = secondIndex;
+	secondIndex = temp;
+	results.splice(secondIndex, 1, valueToAdd);
+
+	return pascal(n - 1, results, sampleArray, index, secondIndex + 1);
 };
 
-console.log(pascal(4));
+// [1]= [1,1]
+
+console.log(pascal(5));
 
 // Do not edit below this line
 module.exports = pascal;
